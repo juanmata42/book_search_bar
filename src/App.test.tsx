a6fde13bdb4130b21bafd09962e751463b73a1f5
juanmata42 <81301176+juanmata42@users.noreply.github.com>
2022-01-06 12:25:45 +0100

    cleanup and base settings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+describe('App', () => {
 
-test('renders learn react link', () => {
+test('renders search bar in english', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //mock function to change language
+  const searchBar = screen.getByPlaceholderText(/Search/i);
+  expect(searchBar).toBeInTheDocument();
+});
 });
